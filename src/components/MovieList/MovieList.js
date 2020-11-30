34d363ff07579fe39aaf6d92752926e0d1ced4cb
jlaguilargomez <jlaguigom@gmail.com>
2020-11-30 19:16:18 +0100

    Check if there is not movies

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -7,18 +7,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const MovieList = (props) => {
   const ManageFavouritesComponent = props.manageFavouritesComponent;
 
-  return (
-    <>
-      {props.movies.map((movie, index) => (
-        <div key={index} className='image-container d-flex justify-content-start m-3'>
-          <img src={movie.Poster} alt='movie'></img>
-          <div onClick={()=> props.handleFavouritesClick(movie)} className='overlay d-flex align-items-center justify-content-center'>
-            <ManageFavouritesComponent action={props.action}></ManageFavouritesComponent>
-          </div>
-        </div>
-      ))}
-    </>
-  );
+  if (props.movies) {
+     return (
+       <>
+         {props.movies.map((movie, index) => (
+           <div key={index} className='image-container d-flex justify-content-start m-3'>
+             <img src={movie.Poster} alt='movie'></img>
+             <div onClick={() => props.handleFavouritesClick(movie)} className='overlay d-flex align-items-center justify-content-center'>
+               <ManageFavouritesComponent action={props.action}></ManageFavouritesComponent>
+             </div>
+           </div>
+         ))}
+       </>
+     );
+  } else {
+    return (<h3>No hay peliculas</h3>);
+  }
+ 
 }
 
 export default MovieList;
